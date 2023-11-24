8c97f9008e5b36c3dd5d80e02338e381930dbdf2
DiegoCamargo1985 <diegointernacional2017@gmail.com>
2023-11-24 11:53:43 -0500

    acutalizacion modulo show

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -11,37 +11,37 @@
                 <div class="card">
                     <div class="card-header">
                         <div class="float-left">
-                            <span class="card-title">{{ __('Show') }} Event</span>
+                            <span class="card-title">{{ __('Mostrar') }} Evento</span>
                         </div>
                         <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('events.index') }}"> {{ __('Back') }}</a>
+                            <a class="btn btn-primary" href="{{ route('events.index') }}"> {{ __('Volver') }}</a>
                         </div>
                     </div>
 
                     <div class="card-body">
-                        
+
                         <div class="form-group">
-                            <strong>Place:</strong>
+                            <strong>Lugar:</strong>
                             {{ $event->place }}
                         </div>
                         <div class="form-group">
-                            <strong>Title:</strong>
+                            <strong>Titulo:</strong>
                             {{ $event->title }}
                         </div>
                         <div class="form-group">
-                            <strong>Description:</strong>
+                            <strong>Descripcion:</strong>
                             {{ $event->description }}
                         </div>
                         <div class="form-group">
-                            <strong>Date Start:</strong>
+                            <strong>Fecha Inicio:</strong>
                             {{ $event->date_start }}
                         </div>
                         <div class="form-group">
-                            <strong>Date End:</strong>
+                            <strong>Fecha Final:</strong>
                             {{ $event->date_end }}
                         </div>
                         <div class="form-group">
-                            <strong>Importance Range:</strong>
+                            <strong>Rango Importancia:</strong>
                             {{ $event->importance_range }}
                         </div>
 
