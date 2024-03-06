ba681446d629ebf8edf11b975c7b63165a388fa2
Maria28903 <mariaperez28903@gmail.com>
2024-03-06 15:47:23 -0500

    correccion error web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
