cb889fbb7247534dadb8c87102653d345f47a5d0
jarcor1995 <egaravito258@gmail.com>
2024-03-19 10:40:24 -0500

    actualizacion 19-03-2024-edwin

---
+++ database/migrations/2023_11_14_125037_create_units_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->enum('unit_type', ['unidad', 'docena', 'centena', 'mil','mm', 'cm', 'm','cm2', 'm2'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+};
