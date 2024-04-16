4d13f3115beb9f83ca3b85b7de8c51c08acf5916
DIEGO CAMARGO <diegointernacional2017@gmail.com>
2024-04-15 19:34:44 -0500

    lo siconos de moba y tu arte de la carpeta mobaMenu, fueron direccionador al welcome

--- resources/views/mobaMenu/Contacto/index.blade.php
+++ resources/views/mobaMenu/Contacto/index.blade.php
@@ -16,7 +16,9 @@
 
     <nav class="navbar">
         <div class="container-fluid">
-            <img src="{{ asset('Imagenes/Logomoba.png') }}" class="navbar-img-left" alt="Logo Moba">
+        <a href="{{ asset('/') }}">
+                <img src="{{ asset('Imagenes/Logomoba.png') }}" class="navbar-img-left" alt="Logo Moba">
+            </a>
             <div class="navbar-buttons">
                 <div class="dropdown">
                     <a href="{{route('mobaMenu.servicios.index')}}">
@@ -34,7 +36,9 @@
                 <a href="#" class="btn btn-primary">Equipo de trabajo</a>
                 <a href="#" class="btn btn-primary">Contáctanos</a>
             </div>
-            <img src="{{ asset('Imagenes/LogoTuArte.png') }}" class="navbar-img-right" alt="Logo Tu Arte">
+            <a href="{{ asset('/') }}">
+                <img src="{{ asset('Imagenes/LogoTuArte.png') }}" class="navbar-img-right" alt="Logo Tu Arte">
+            </a>
         </div>
     </nav>
 
