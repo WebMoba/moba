1087b13f047ea1534e70164b941e171cd598a1dd
DIEGO CAMARGO <diegointernacional2017@gmail.com>
2024-04-15 20:16:58 -0500

    creacion carpetas imagenes moba y tu arte

--- resources/views/tuArteMenu/categorias/index.blade.php
+++ resources/views/tuArteMenu/categorias/index.blade.php
@@ -53,9 +53,18 @@
         <!-- Contenido de la página aquí -->
         
        <div class="container">
-
+       <div class="contenedor">
+        <div class="campo campo1">Mascotas</div>
+        <div class="contenedor-columna">
+            <div class="campo campo2">Accesorios</div>
+            <div class="campo campo3">Decoracion</div>
+        </div>
+        <div class="campo campo4">Joditas pal recuerdo</div>
+        </div>
        </div>
 
+
+       
     <div class="vertical-line right-line">
         <hr class="linea2">
         <a href="https://www.instagram.com/moba_agencia"><i class="bi bi-instagram"></i></a>
@@ -111,8 +120,46 @@
 }
 
 .container{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+
     border: solid 3px yellow;
     width: 90%;
     height: 100vh;
 }
-</style>
+.contenedor {
+  display: flex;
+  height: 80%; /* 100% de la altura de la ventana */
+  width: 90%;
+  margin-top: 5%;
+
+}
+
+.contenedor-columna {
+  display: flex;
+  flex-direction: column; /* Apila los elementos verticalmente */
+  flex: 1; /* El contenedor-columna ocupa el espacio restante */
+}
+
+.campo {
+  border: 15px solid white;
+  color: white;
+}
+
+.campo1 {
+  flex: 0 0 30%; /* No crecerá, no se encogerá, 30% de ancho */
+  height: 100%; /* 100% de altura */
+}
+
+.campo2,
+.campo3 {
+  flex: 1; /* Los campos 2 y 3 se expanden para ocupar el espacio disponible */
+}
+
+.campo4 {
+  flex: 0 0 30%; /* No crecerá, no se encogerá, 30% de ancho */
+  height: 100%; /* 100% de altura */
+}
+    </style>
