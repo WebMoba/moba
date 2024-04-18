a99e0082dc131d4d3963156f096429049bf0281f
Maria28903 <mariaperez28903@gmail.com>
2024-04-18 10:42:07 -0500

    Creacion de carpeta para las img de tuArte

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -90,37 +90,37 @@ class PurchaseController extends Controller
      */
 
 
-     public function store(Request $request)
-{
-    // Acceder a los datos enviados desde el formulario
-    $datos = $request->input('data');
+    public function store(Request $request)
+    {
+        // Acceder a los datos enviados desde el formulario
+        $datos = $request->input('data');
 
-    // Crear una nueva compra
-    $compra = new Purchase();
-    $compra->name = $datos['nombre_proveedor'];
-    $compra->date = $datos['fecha'];
-    $compra->people_id = $datos['proveedor_id'];
-    $compra->save();
+        // Crear una nueva compra
+        $compra = new Purchase();
+        $compra->name = $datos['nombre_proveedor'];
+        $compra->date = $datos['fecha'];
+        $compra->people_id = $datos['proveedor_id'];
+        $compra->save();
 
-    // Recorrer los detalles de la compra y guardarlos en la base de datos
-    foreach ($datos['detalles'] as $detalle) {
-        $detalleCompra = new DetailPurchase();
-        $detalleCompra->quantity = $detalle['cantidad'];
-        $detalleCompra->price_unit = $detalle['precio_unitario'];
-        $detalleCompra->subtotal = $detalle['subtotal'];
-        $detalleCompra->discount = $detalle['descuento'];
-        $detalleCompra->total = $detalle['total'];
-        $detalleCompra->materials_raws_id = $detalle['materia_prima'];
-        $detalleCompra->purchases_id = $compra->id; // Asociar el detalle con la compra creada
-        $detalleCompra->save();
+        // Recorrer los detalles de la compra y guardarlos en la base de datos
+        foreach ($datos['detalles'] as $detalle) {
+            $detalleCompra = new DetailPurchase();
+            $detalleCompra->quantity = $detalle['cantidad'];
+            $detalleCompra->price_unit = $detalle['precio_unitario'];
+            $detalleCompra->subtotal = $detalle['subtotal'];
+            $detalleCompra->discount = $detalle['descuento'];
+            $detalleCompra->total = $detalle['total'];
+            $detalleCompra->materials_raws_id = $detalle['materia_prima'];
+            $detalleCompra->purchases_id = $compra->id; // Asociar el detalle con la compra creada
+            $detalleCompra->save();
+        }
+
+        // Retornar una respuesta de redirección
+        return redirect()->route('purchases.index')
+            ->with('success', 'Registro creado exitosamente');
     }
 
-    // Retornar una respuesta de redirección
-    return redirect()->route('purchases.index')
-        ->with('success', 'Registro creado exitosamente');
-}
 
-    
 
     /**
      * Display the specified resource.
@@ -135,10 +135,6 @@ class PurchaseController extends Controller
             $person = Person::find($id);
             return "$id_card - $person->addres";
         })->toArray();
-
-        
-
-        
 
         $purchases = Purchase::pluck('name', 'id');
         $materialsRaws = MaterialsRaw::pluck('name', 'id');
@@ -176,13 +172,6 @@ class PurchaseController extends Controller
 
         return view('purchase.create', compact('purchase', 'people', 'detailPurchase', 'purchases', 'materialsRaws', 'purchaseName', 'confirm'));
     }
-
-
-
-
-
-
-
 
     /**
      * Update the specified resource in storage.
--- resources/views/purchase/show.blade.php
+++ resources/views/purchase/show.blade.php
@@ -21,18 +21,7 @@
                     <div class="card-body">
                         <h2>Compra</h2>
 
-                        <div class="form-group">
-                            <strong>Nombre dell proveedor:</strong>
-                            {{ $purchase->name }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Fecha de compra:</strong>
-                            {{ $purchase->date }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Nº de identificación y dirección:</strong>
-                            {{ $purchase->person->id_card }} - {{ $purchase->person->addres }}
-                        </div>
+                        
 
 
                         <h2>Detalles de la compra</h2>
