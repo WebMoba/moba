5702201849a670a20408b991a7f858507b301480
DIEGO CAMARGO <diegointernacional2017@gmail.com>
2024-04-22 17:46:45 -0500

    modificacion estilos welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,4 +181,16 @@
         .linea.grey{
             border-left: 1px solid #BCCCE0;
         }
+
+
+
+
+        @keyframes spin-horizontal {
+    0%, 100% { transform: rotateY(0deg); } /* La imagen está en su posición original */
+    50% { transform: rotateY(180deg); } /* La imagen gira 180 grados */
+}
+.logos img {
+    animation: spin-horizontal 8s linear infinite; /* La animación dura 4 segundos, es lineal y se repite infinitamente */
+}
+
 </style>
