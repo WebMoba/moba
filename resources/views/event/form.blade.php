43b99d149b1155a2a35701b448cc7073be0ff377
DIEGO CAMARGO <diegointernacional2017@gmail.com>
2024-04-24 21:41:46 -0500

    modificacion botones tabla personas

--- resources/views/event/form.blade.php
+++ resources/views/event/form.blade.php
@@ -35,7 +35,8 @@
     </div>
     <br>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary" onclick="return confirm('¿Está seguro de que desea Crear el evento?')">{{ __('Crear') }}</button>
+        <button type="submit" class="btn btn-primary" 
+        >{{ __('Crear') }}</button>
     </div>
 </div>
 
--- resources/views/person/form.blade.php
+++ resources/views/person/form.blade.php
@@ -69,7 +69,7 @@
 
     </div>
     <div class="box-footer mt20">
-        <br><button type="submit" class="btn btn-primary" onclick="return confirm('¿Está seguro de que desea crear a la persona?')">{{ __('Crear') }}</button>
+        <br><button type="submit" class="btn btn-primary" >{{ __('Crear') }}</button>
     </div>
 </div>
 <script>
