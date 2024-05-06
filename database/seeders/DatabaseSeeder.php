b89673599b38b65405e77b41767f947e0e118ab6
Maria28903 <mariaperez28903@gmail.com>
2024-05-06 15:21:06 -0500

    .env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,17 +22,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
 
-        DB::table('people')->insert([
-            'id_card' => '1234567890',
-            'addres' => 'Calle 15 # 7A - 30',
-            'identification_type' => 'NIT',
-            'name' => 'Agencia Moba',
-            'rol' => 'Administrador',
-            'region_id' => 15,
-            'disable' => false,
-            'number_phones_id' => 4,
-            'towns_id' => 923,
-            'users_id' => 6,
-        ]);
+        
     }
 }
