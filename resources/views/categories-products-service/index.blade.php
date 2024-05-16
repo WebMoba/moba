d49e7c6a8fc8916c8c49ba7037d9b8cd1b766bd8
jarcor1995 <egaravito258@gmail.com>
2024-05-16 16:54:02 -0500

    cambios a proyectosedwin

--- resources/views/categories-products-service/index.blade.php
+++ resources/views/categories-products-service/index.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Categorias'])
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
@@ -44,7 +45,8 @@
                             </div>
                         </div>
                     </div>
-                    @include('layouts.navbars.auth.topnav', ['title' => 'Categorias'])
+
+
                     @if ($message = Session::get('success'))
                         <div class="alert alert-success text-center">
                             <p>{{ $message }}</p>
@@ -56,62 +58,60 @@
                         </div>
                     @endif
                     <div class="card-body">
-                        <div class="card-body">
 
-                            <div class="table-responsive">
-                                <table class="table table-striped table-hover">
-                                    <thead class="thead">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+
+                                        <th>Nombre</th>
+                                        <th>Descripcion</th>
+                                        <th>Estado</th>
+                                        <th>Cantidad</th>
+                                        <th>Popularidad</th>
+                                        <th>Tipo</th>
+
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($categoriesProductsServices as $categoriesProductsService)
                                         <tr>
-                                            <th>No</th>
+                                            <td>{{ ++$i }}</td>
 
-                                            <th>Nombre</th>
-                                            <th>Descripcion</th>
-                                            <th>Estado</th>
-                                            <th>Cantidad</th>
-                                            <th>Popularidad</th>
-                                            <th>Tipo</th>
+                                            <td>{{ $categoriesProductsService->name }}</td>
+                                            <td>{{ $categoriesProductsService->description }}</td>
+                                            <td>{{ $categoriesProductsService->status }}</td>
+                                            <td>{{ $categoriesProductsService->quantity }}</td>
+                                            <td>{{ $categoriesProductsService->popular }}</td>
+                                            <td>{{ $categoriesProductsService->type }}</td>
 
-                                            <th></th>
+                                            <td>
+
+                                                <form class="frData"
+                                                    action="{{ route('categories-products-service.destroy', $categoriesProductsService->id) }}"
+                                                    method="POST"
+                                                    data-disable="{{ $categoriesProductsService->disable }}">
+                                                    <a class="btn btn-sm btn-primary {{ $categoriesProductsService->disable ? 'disabled' : '' }}"
+                                                        href="{{ route('categories-products-service.show', $categoriesProductsService->id) }}">
+                                                        <i class="bi bi-eye-fill"></i></a>
+                                                    <a class="btn btn-sm btn-success {{ $categoriesProductsService->disable ? 'disabled' : '' }}"
+                                                        href="{{ route('categories-products-service.edit', $categoriesProductsService->id) }}">
+                                                        <i class="bi bi-pencil-square"></i></a>
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger btn-sm">
+                                                        {!! $categoriesProductsService->disable
+                                                            ? '<i class="bi bi-check-circle-fill"></i>'
+                                                            : '<i class="bi bi-x-circle"></i>' !!}
+                                                    </button>
+                                                </form>
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($categoriesProductsServices as $categoriesProductsService)
-                                            <tr>
-                                                <td>{{ ++$i }}</td>
-
-                                                <td>{{ $categoriesProductsService->name }}</td>
-                                                <td>{{ $categoriesProductsService->description }}</td>
-                                                <td>{{ $categoriesProductsService->status }}</td>
-                                                <td>{{ $categoriesProductsService->quantity }}</td>
-                                                <td>{{ $categoriesProductsService->popular }}</td>
-                                                <td>{{ $categoriesProductsService->type }}</td>
-
-                                                <td>
-
-                                                    <form class="frData"
-                                                        action="{{ route('categories-products-service.destroy', $categoriesProductsService->id) }}"
-                                                        method="POST"
-                                                        data-disable="{{ $categoriesProductsService->disable }}">
-                                                        <a class="btn btn-sm btn-primary {{ $categoriesProductsService->disable ? 'disabled' : '' }}"
-                                                            href="{{ route('categories-products-service.show', $categoriesProductsService->id) }}">
-                                                            <i class="bi bi-eye-fill"></i></a>
-                                                        <a class="btn btn-sm btn-success {{ $categoriesProductsService->disable ? 'disabled' : '' }}"
-                                                            href="{{ route('categories-products-service.edit', $categoriesProductsService->id) }}">
-                                                            <i class="bi bi-pencil-square"></i></a>
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm">
-                                                            {!! $categoriesProductsService->disable
-                                                                ? '<i class="bi bi-check-circle-fill"></i>'
-                                                                : '<i class="bi bi-x-circle"></i>' !!}
-                                                        </button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                     {!! $categoriesProductsServices->links() !!}
