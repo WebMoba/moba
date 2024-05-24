b49e101f9acc91b30e7629c08362a70bc464c785
Maria28903 <mariaperez28903@gmail.com>
2024-05-24 16:23:29 -0500

    Mejora de vista MaterialRaw

--- resources/views/materials-raw/show.blade.php
+++ resources/views/materials-raw/show.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('template_title')
-    {{ $materialsRaw->name ??  __('Show Materials Raw') }}
+    {{ $materialsRaw->name ?? __('Show Materials Raw') }}
 @endsection
 
 @section('content')
-@include('layouts.navbars.auth.topnav', ['title' => 'Mostrar Materia Prima'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Mostrar Materia Prima'])
     <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
@@ -23,15 +23,11 @@
                             {{ $materialsRaw->name }}
                         </div>
                         <div class="form-group">
-                            <strong>Cantidad existente:</strong>
-                            {{ $materialsRaw->existing_quantity }}
-                        </div>
-                        <div class="form-group">
                             <strong>Tipo de unidad:</strong>
                             {{ $materialsRaw->unit->unit_type }}
                         </div>
                         <a class="btn btn-primary" href="{{ route('materials_raws.index') }}"><i
-                            class="bi bi-arrow-left-circle"></i><span class="tooltiptext">Volver</span></a>
+                                class="bi bi-arrow-left-circle"></i><span class="tooltiptext">Volver</span></a>
                     </div>
                 </div>
             </div>
