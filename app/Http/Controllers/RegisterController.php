60bd67858800ca8ed5cde2a805ab9ff36d724b1a
DIEGO CAMARGO <diegointernacional2017@gmail.com>
2024-06-07 11:41:37 -0500

    modificacion pasworrd yfondo tablas

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-// use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Carbon\Carbon;
 
 class RegisterController extends Controller
 {
-
     public function create()
     {
         return view('auth.register');
@@ -18,14 +16,28 @@ class RegisterController extends Controller
     {
         $attributes = request()->validate([
             'name' => 'required|max:255|min:2',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|min:5|max:255',
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                'unique:users,email',
+                'regex:/^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com|yahoo\.com|outlook\.com)$/'
+            ],
+            'password' => [
+                'required',
+                'min:5',
+                'max:255',
+                'regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/'
+            ],
             'terms' => 'required'
+        ], [
+            'email.regex' => 'El correo electrónico debe ser de los dominios: @gmail.com, @hotmail.com, @yahoo.com o @outlook.com.',
+            'password.regex' => 'La contraseña debe contener al menos una letra mayúscula, letras minúsculas y números.',
         ]);
+
         $user = User::create($attributes);
         auth()->login($user);
 
         return redirect('/dashboard');
     }
-
 }
